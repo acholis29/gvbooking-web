98fa6ffe3eddc6a8a5cfaed42bd28e6ed0579688
Riki Sipahelut <rikisipahelut@gmail.com>
2025-07-29 16:04:23 +0800

    Card Accordion

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -5,6 +5,7 @@ import SkeletonCardHorizontal from "@/components/SkeletonCardHorizontal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInbox } from "@fortawesome/free-solid-svg-icons";
 import Breadcrumb from "@/components/Breadcrumb";
+import CardAccordion from "@/components/CardAccordion";
 
 type CartItem = {
   idx_comp: string;
@@ -38,81 +39,10 @@ export default function Cart() {
     // Cart Page
     <div className="max-w-screen-xl mx-auto">
       <Breadcrumb pageName="Cart" />
-      {ListCart.length > 0 ? (
-        <section className="flex flex-col md:flex-row p-6 bg-white gap-1">
-          {/* Konten Kiri */}
-          <div className="md:w-4/6 text-gray-700">
-            {isLoading ? (
-              <>
-                <SkeletonCardHorizontal />
-              </>
-            ) : ListCart.length > 0 ? (
-              ListCart.map((item, index) => (
-                <HorizontalCard
-                  key={index}
-                  idx_comp={item.idx_comp}
-                  idx_excursion={item.idx_excursion}
-                  image="/images/destination/tanah-lot/tanah-lot6.jpg"
-                  title={item.title}
-                  sub_title={item.sub_title}
-                  link="/cart"
-                  currency={`${item.currency}`}
-                  price={`${item.price}`}
-                  onDelete={loadCart} // ✅ ini dikirim ke anak
-                />
-              ))
-            ) : (
-              <div className="col-span-4 text-center text-gray-500 py-10">
-                <FontAwesomeIcon
-                  icon={faInbox}
-                  className="w-4 h-4 text-gray-600 mr-2"
-                />
-                Data tidak ditemukan...
-              </div>
-            )}
-          </div>
-          {/* Konten Kanan */}
-          <div className="md:w-2/6 text-black">
-            <div className="border p-3 rounded-2xl bg-gray-600">
-              <div className="flex flex-row justify-between">
-                <div className="">
-                  <p className="font-bold text-lg  text-white">
-                    Total Item ( {ListCart.length} )
-                  </p>
-                </div>
-                <div className="">
-                  <p className="font-bold text-lg  text-white">
-                    {/* Total */}
-                    {ListCart[0].currency}{" "}
-                    {ListCart.reduce(
-                      (total, item) => total + Number(item.price),
-                      0
-                    ).toLocaleString("id-ID")}
-                  </p>
-                  <p className="text-sm  text-white">
-                    *include tax and service
-                  </p>
-                </div>
-              </div>
-
-              <button
-                type="button"
-                className="mt-3 md:mt-10 w-full text-white bg-red-gvi hover:bg-red-900 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-              >
-                PAYMENT
-              </button>
-            </div>
-          </div>
-        </section>
-      ) : (
-        <div className="col-span-4 text-center text-gray-500 py-10 my-30">
-          <FontAwesomeIcon
-            icon={faInbox}
-            className="w-4 h-4 text-gray-600 mr-2"
-          />
-          Cart is empty
-        </div>
-      )}
+      <section className="flex flex-col p-6 bg-white gap-1">
+        <CardAccordion />
+        <CardAccordion />
+      </section>
     </div>
   );
 }
---
+++ src/components/CardAccordion.tsx
@@ -0,0 +1,161 @@
+// components/CardAccordion.tsx
+import React, { useState } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionPanel,
+  AccordionTitle,
+} from "flowbite-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowAltCircleDown,
+  faCalendar,
+  faCalendarDays,
+  faChevronDown,
+  faEdit,
+  faTrash,
+  faUsers,
+} from "@fortawesome/free-solid-svg-icons";
+
+type CardAccordionProps = {
+  bgColor?: string;
+  textColor?: string;
+  title?: string;
+};
+
+const CardAccordion: React.FC<CardAccordionProps> = ({
+  bgColor = "bg-gray-500",
+  textColor = "text-white",
+  title = "Badge",
+}) => {
+  const [isOpenAccordion, setAccordion] = useState(false);
+  return (
+    <div className="relative md:max-w-3xl mb-4">
+      {/* Tombol pojok kanan atas */}
+      <button
+        className="absolute top-2 right-2 z-10  text-white text-sm px-1 py-1 rounded hover:bg-gray-100"
+        onClick={() => {
+          // aksi untuk toggle accordion
+          console.log("toggle accordion");
+          setAccordion(!isOpenAccordion);
+        }}
+      >
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          className="w-10 h-10 text-gray-500"
+          size="lg"
+        />
+      </button>
+      <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-50">
+        <div className="flex items-center flex-row">
+          <img
+            className="object-cover w-full rounded-tl-sm h-96 md:h-auto md:w-48"
+            src="/images/destination/indonesia.jpg"
+            alt=""
+          />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+              NUSA PENIDAS OUTKUTSE
+            </h5>
+            <div className="flex flex-row"></div>
+            <p className="mb-3 font-normal text-gray-700 ">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-row space-x-4 w-full bg-gray-100">
+          <div className="basis-1/4 grow p-4 text-left">
+            <p className="text-black text-sm font-bold">SUB TOTAL</p>
+            <p className="text-red-700 text-sm font-semibold">IDR 2.000.000</p>
+          </div>
+          <div className="basis-3/8 grow p-4 text-left">
+            <p className="text-black text-sm font-bold">DISC</p>
+            <p className="text-red-700 text-sm font-semibold">3.00</p>
+          </div>
+          <div className="basis-3/8 grow p-4 text-left">
+            <p className="text-black text-sm font-bold">TOTAL</p>
+            <p className="text-red-700 text-sm font-semibold">2.000.000</p>
+          </div>
+        </div>
+        <div
+          className={`${isOpenAccordion ? "flex flex-row" : "hidden"} w-full`}
+        >
+          <div className="w-[5%] py-4 text-left">
+            <FontAwesomeIcon
+              icon={faCalendarDays}
+              className="w-10 h-10 text-gray-500"
+              size="sm"
+            />
+          </div>
+          <div className="w-[95%] p-4  text-left">
+            <p className="text-black text-xs font-bold">Pickup date</p>
+            <p className="text-black text-xs">Sunday, 30 July 2025</p>
+            <hr className="my-2 border border-gray-400 opacity-50" />
+            <p className="text-black text-xs font-bold">Adult</p>
+            <p className="text-black text-xs">1 Adult</p>
+            <hr className="my-2 border border-gray-400 opacity-50" />
+            <p className="text-black text-xs font-bold">Room Number</p>
+            <p className="text-black text-xs">1 Child</p>
+            <hr className="my-2 border border-gray-400 opacity-50" />
+          </div>
+        </div>
+        <div
+          className={`${isOpenAccordion ? "flex flex-row" : "hidden"} w-full`}
+        >
+          <div className="w-[5%] py-4  text-left">
+            <FontAwesomeIcon
+              icon={faUsers}
+              className="w-10 h-10 text-gray-500"
+              size="sm"
+            />
+          </div>
+          <div className="w-[95%] p-4  text-left">
+            <p className="text-black text-xs font-bold">Traveler</p>
+            <p className="text-black text-xs font-bold">1 Person</p>
+            <hr className="my-2 border border-gray-400 opacity-50" />
+            <div className="flex flex-row justify-between">
+              <p className="text-black text-xs">1 Adult</p>
+              <p className="text-black text-xs font-bold">IDR 0.00</p>
+            </div>
+            <hr className="my-2 border border-gray-400 opacity-50" />
+            <div className="flex flex-row justify-between">
+              <p className="text-black text-xs">1 Service</p>
+              <p className="text-black text-xs font-bold">IDR 2.000.000.00</p>
+            </div>
+            <hr className="my-2 border border-gray-400 opacity-50" />
+          </div>
+        </div>
+        <div
+          className={`${isOpenAccordion ? "flex flex-row" : "hidden"} w-full`}
+        >
+          <div className="w-[5%] py-4  text-left"></div>
+          <div className="w-[95%] p-4  flex flex-row justify-start items-center gap-3">
+            <a href="#" className="flex flex-row items-center gap-2 group">
+              <FontAwesomeIcon
+                icon={faTrash}
+                className="w-5 h-5 text-gray-500 group-hover:text-red-700"
+                size="sm"
+              />
+              <p className="text-gray-600 text-sm group-hover:text-red-700">
+                Remove
+              </p>
+            </a>
+            <a href="#" className="flex flex-row items-center gap-2 group">
+              <FontAwesomeIcon
+                icon={faEdit}
+                className="w-5 h-5 text-gray-500 group-hover:text-red-700"
+                size="sm"
+              />
+              <p className="text-gray-600 text-sm group-hover:text-red-700">
+                Change
+              </p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardAccordion;
