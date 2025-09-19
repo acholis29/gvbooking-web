cfd8bae30f6b60badac19e5a5e71f1cd115193d9
Riki Sipahelut <rikisipahelut@gmail.com>
2025-09-19 14:11:20 +0800

    SignIn dan SignOut Drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 // Library
+import { signIn, signOut, useSession } from "next-auth/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCheck,
@@ -17,6 +18,8 @@ import {
   faMoneyCheckDollar,
   faShoppingCart,
   faSign,
+  faSignIn,
+  faSignOut,
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
 // Context State Global
@@ -30,7 +33,7 @@ import { useProfile } from "@/context/ProfileContext";
 // Component
 import ModalComponent from "./ModalComponent";
 // Helper
-import { capitalizeWords } from "@/helper/helper";
+import { capitalizeWords, truncateText } from "@/helper/helper";
 
 export default function DrawerComponent({
   isOpen,
@@ -59,6 +62,8 @@ export default function DrawerComponent({
   const [menuSelected, setMenuSelected] = useState("");
   const pathname = usePathname();
   const hideCartIcon = pathname === "/" || pathname === "/home";
+  // Login with Google
+  const { data: session, status } = useSession();
 
   useEffect(() => {
     fetch("/api/currency", {
@@ -128,6 +133,27 @@ export default function DrawerComponent({
         </button>
         <div className="py-4 overflow-y-auto">
           <ul className="space-y-2 font-medium">
+            {status == "unauthenticated" && ( // kalau belum login, tampilkan li
+              <li>
+                <Link
+                  href="#"
+                  className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+                  onClick={() => {
+                    openModal();
+                    setMenuSelected("Sign In");
+                  }}
+                >
+                  <FontAwesomeIcon
+                    icon={faSignIn}
+                    className="w-5 h-5 text-lg text-gray-400"
+                  />
+                  <span className="flex-1 ms-3 text-gray-500 whitespace-nowrap">
+                    Sign In
+                  </span>
+                </Link>
+              </li>
+            )}
+
             <li>
               <button
                 type="button"
@@ -143,7 +169,16 @@ export default function DrawerComponent({
                   className="w-5 h-5 text-xl text-gray-400"
                 />
                 <span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap text-gray-500">
-                  {firstName == "" ? "Profile" : capitalizeWords(firstName)}
+                  {" "}
+                  {status == "authenticated"
+                    ? truncateText(
+                        session.user?.name?.toLocaleUpperCase() ?? "",
+                        10
+                      )
+                    : firstName == ""
+                    ? "PROFILE"
+                    : firstName.toUpperCase()}
+                  {/* {firstName == "" ? "Profile" : capitalizeWords(firstName)} */}
                 </span>
                 <FontAwesomeIcon
                   icon={faChevronDown}
@@ -256,24 +291,28 @@ export default function DrawerComponent({
                 )}
               </Link>
             </li>
-            <li>
-              <Link
-                href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100  group"
-                onClick={() => {
-                  const drawer = document.getElementById("drawer-navigation");
-                  if (drawer) drawer.classList.add("translate-x-full");
-                }}
-              >
-                <FontAwesomeIcon
-                  icon={faSign}
-                  className="w-5 h-5 text-lg text-gray-400"
-                />
-                <span className="flex-1 ms-3 text-gray-500 whitespace-nowrap">
-                  Sign Up
-                </span>
-              </Link>
-            </li>
+            {status == "authenticated" && (
+              <li>
+                <Link
+                  href="#"
+                  className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100  group"
+                  onClick={() => {
+                    signOut();
+                    sessionStorage.setItem("oauth", "false");
+                    const drawer = document.getElementById("drawer-navigation");
+                    if (drawer) drawer.classList.add("translate-x-full");
+                  }}
+                >
+                  <FontAwesomeIcon
+                    icon={faSignOut}
+                    className="w-5 h-5 text-lg text-gray-400"
+                  />
+                  <span className="flex-1 ms-3 text-gray-500 whitespace-nowrap">
+                    Sign Out
+                  </span>
+                </Link>
+              </li>
+            )}
           </ul>
         </div>
       </div>
@@ -290,6 +329,20 @@ export default function DrawerComponent({
         <ModalComponent title="Language" icon={faMoneyCheckDollar}>
           {/* <LanguageContent /> */}
           <LanguageContent languages={masterLanguage} />
+        </ModalComponent>
+      )}
+
+      {menuSelected == "Language" && (
+        <ModalComponent title="Language" icon={faMoneyCheckDollar}>
+          {/* <LanguageContent /> */}
+          <LanguageContent languages={masterLanguage} />
+        </ModalComponent>
+      )}
+
+      {menuSelected == "Sign In" && (
+        <ModalComponent title="Sign In" icon={faUser}>
+          {/* <LanguageContent languages={languageMaster} /> */}
+          <SignInContent />
         </ModalComponent>
       )}
     </>
@@ -366,5 +419,106 @@ const LanguageContent = ({
         </li>
       ))}
     </ul>
+  );
+};
+
+const SignInContent = () => {
+  const { closeModal } = useModal();
+
+  return (
+    <div>
+      <form className="max-w-sm mx-auto">
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="name@flowbite.com"
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Your password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+          />
+        </div>
+        <div className="flex items-start mb-5">
+          <div className="flex items-center h-5">
+            <input
+              id="remember"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300"
+              required
+            />
+          </div>
+          <label
+            htmlFor="remember"
+            className="ms-2 text-sm font-medium text-gray-900"
+          >
+            Remember me
+          </label>
+        </div>
+
+        {/* tombol submit biasa */}
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center"
+        >
+          Sign In
+        </button>
+
+        {/* separator */}
+        <div className="flex items-center my-4">
+          <hr className="flex-grow border-gray-300" />
+          <span className="mx-2 text-gray-500 text-sm">OR</span>
+          <hr className="flex-grow border-gray-300" />
+        </div>
+
+        {/* tombol login dengan google */}
+        <button
+          type="button"
+          // onClick={() => console.log("Login with Google clicked")}
+          onClick={() => {
+            signIn("google");
+            sessionStorage.setItem("oauth", "true");
+          }}
+          className="flex items-center justify-center w-full px-5 py-2.5 border border-gray-300 rounded-lg text-sm font-medium text-gray-700 hover:bg-gray-100"
+        >
+          <img
+            src="https://www.svgrepo.com/show/475656/google-color.svg"
+            alt="Google logo"
+            className="w-5 h-5 mr-2"
+          />
+          Continue with Google
+        </button>
+
+        {/* link ke sign up */}
+        <p className="text-sm text-center text-gray-600 mt-4">
+          Don’t have an account?{" "}
+          <a
+            href="/signup"
+            className="text-blue-600 hover:underline font-medium"
+          >
+            Sign Up
+          </a>
+        </p>
+      </form>
+    </div>
   );
 };
