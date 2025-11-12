f02e5efc16c3ca5be223c1a865588a130c3d56dc
Riki Sipahelut <rikisipahelut@gmail.com>
2025-11-12 15:48:01 +0800

    Details - Badge Sub Excursion

--- src/components/ProductSubNewCard.tsx
+++ src/components/ProductSubNewCard.tsx
@@ -532,11 +532,11 @@ const ProductSubNew: React.FC<ProductSubNewProps> = ({
                   return (
                     <span
                       key={index}
-                      className={`bg-gray-300 hover:bg-amber-600 text-gray-800 text-xs font-semibold me-2 px-2.5 py-1 rounded-md ${
+                      className={` text-gray-800 text-xs font-semibold me-2 px-2.5 py-1 rounded-md ${
                         items.mandatory.toLocaleLowerCase() == "true"
                           ? "border border-red-700"
-                          : "cursor-pointer"
-                      } ${isExist ? "border border-red-700" : ""}`}
+                          : "cursor-pointer border border-amber-50 hover:border-amber-500" // mandatory false
+                      } ${isExist ? "bg-amber-500" : "bg-gray-300"}`}
                       onClick={() => {
                         if (items.mandatory.toLocaleLowerCase() != "true") {
                           handleBadgeSurchargeChange(items);
